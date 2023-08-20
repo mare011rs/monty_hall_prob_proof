4b8e66a564288541ade42eb875892bd860c4a6b6
mare011rs <mare011rs@gmail.com>
2023-08-20 20:44:50 +0200

    update naming and comments

--- monty_hall.py
+++ monty_hall.py
@@ -14,22 +14,29 @@ while counter > 0:
     random.shuffle(items)
 
     # Taking a random choice from 3 items
+    # This has 1/3 chance to be correct
     first_choice_index = random.randrange(0, 2)
     first_choice_sum += items[first_choice_index]
 
+    # Remaining items that weren't chosen have 2/3 chance to have a correct answer
+
     # Finding an index of a wrong item that's not the first chosen item
-    spoof_index = 0
+    # First encountered wrong item is marked by taking its index
+    wrong_choice_index = 0
     for i, item in enumerate(items):
         if i == first_choice_index:
             pass
         elif item == 0:
-            spoof_index = i
+            wrong_choice_index = i
             break
+    
+    # Now that we found the wrong choice out of the remaining two we will remove it
+    # This makes the only remaining item have the 2/3 chance to be correct
 
-    # Finding the second item that which is not the spoof item, nor is it the first chosen item
+    # Finding the second item that which is not the wrong item, nor is it the first chosen item
     second_item_index = 0
     for i, item in enumerate(items):
-        if i in [first_choice_index, spoof_index]:
+        if i in [first_choice_index, wrong_choice_index]:
             pass
         else:
             second_item_index = i
